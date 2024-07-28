2c9906bb95748c9c94156cb2818fa08e9f0ff7c8
Aayush Anand <aayushanand27@gmail.com>
2024-07-28 15:45:30 +0530

    Add files via upload

---
+++ plus_one.java
@@ -0,0 +1,27 @@
+
+import java.util.Arrays;
+
+public class plus_one {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        
+        // Start from the last digit and move backwards
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        
+        // If all digits are 9, we need an extra digit at the beginning
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+
+    public static void main(String[] args) {
+        int digits[] = {4, 3, 2, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+}
