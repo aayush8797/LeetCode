4c716c1ec19f35696ef5ae335d13e8074bf92af1
Aayush Anand <aayushanand27@gmail.com>
2024-08-21 18:33:55 +0530

    Add files via upload

---
+++ Container_with_most_water.java
@@ -0,0 +1,28 @@
+public class Container_with_most_water {
+    public static int maxArea(int[] height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentArea = width * currentHeight;
+            maxArea = Math.max(maxArea, currentArea);
+
+            // Move the pointer pointing to the shorter line
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println("Maximum water a container can store: " + maxArea(height));
+    }
+}
